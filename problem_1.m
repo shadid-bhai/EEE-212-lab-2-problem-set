335da0120f38b406e1a07501eae84488bb841aed
shadid-bhai <90759032+shadid-bhai@users.noreply.github.com>
2026-07-01 20:30:19 +0600

    Add files via upload

---
+++ problem_1.m
@@ -0,0 +1,39 @@
+%clear command window, close prev work
+clc;
+close all;
+clear all; %remove all variables
+%%
+format long; %ans set to 15 decimal digits
+syms f(x); %create symbolic func
+f(x) = x^3-2*x+2;
+%%
+fplot(f, [-10,10]) %plotting x and y, setting y limit to [-10,10]
+ax = gca;
+%setting x and y location to origin
+ax.XAxisLocation = 'origin';
+ax.YAxisLocation = 'origin';
+%turning grid on for both axes
+ax.XGrid = 'on';
+ax.YGrid = 'on';
+%%
+df = diff(f, x); %differentiating w.r. to x
+x_0 = 2; %initial value for the method
+tol = 10e-5;
+n = 50;
+%%
+i = 1;
+while i<=n
+    x_i = x_0 - (f(x_0)/df(x_0)); %applying formula
+    if abs(f(x_i)) < tol
+        disp(double(x_i));
+        break;
+    end
+    x_0 = x_i;
+    i = i+1;
+end
+%%
+a = sym2poly(f); %extract vector of all numberic coefficients
+r = roots(a) %return roots of the polynomial
+
+
+
